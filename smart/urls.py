27b9e424a38bef223312f0aa909f49a4a454f33b
selimonat <onatselim@gmail.com>
2018-08-21 01:03:37 +0200

    bokeh integration for interface monitoring

--- smart/urls.py
+++ smart/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = 'smart'
 urlpatterns = [
     path(''           , views.index,  name='index'),
+    path('test/', views.test, name='test'),
     path('<slug:source>/', views.interface, name='interface')
 ]
--- smart/views.py
+++ smart/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 
 import PoorMansSmartHome.PoorMansSmartHome as p
 import time as t
+
+from bokeh.plotting import figure, output_file, show 
+from bokeh.embed import components
+
 # Create your views here.
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the polls index.")
@@ -13,4 +17,26 @@ def index(request):
     return render(request,'index.html',states)
 
 def interface(request,source):
-    return render(request,'source.html',{'interface':source,'versions':["today","week","month","all"]})
+    #load source's  DF
+    h       = p.Home()
+
+    script,div = h.get_plot(source)
+    return render(request,'source.html',
+            {'script' : script , 'div' : div} )
+
+def test(request):
+    x= [1,3,5,7,9,11,13]
+    y= [1,2,3,4,5,6,7]
+    title = 'y = f(x)'
+
+    plot = figure(title= title , 
+        x_axis_label= 'X-Axis', 
+        y_axis_label= 'Y-Axis')
+
+    plot.line(x, y, legend= 'f(x)', line_width = 2)
+    #Store components 
+    script, div = components(plot)
+
+    #Feed them to the Django template.
+    return render_to_response( 'test.html',
+            {'script' : script , 'div' : div} )
