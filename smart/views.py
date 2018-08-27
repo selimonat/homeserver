6157870e0b50ae3fb11a4ae41ddbf00666f84d6c
selimonat <onatselim@gmail.com>
2018-08-27 20:50:51 +0200

    bokeh rect works, but with no content

--- smart/views.py
+++ smart/views.py
@@ -9,24 +9,36 @@ from bokeh.embed import components
 
 # Create your views here.
 #def index(request):
-#    return HttpResponse("Hello, world. You're at the polls index.")
+	#    return HttpResponse("Hello, world. You're at the polls index.")
 def index(request):
-    h       = p.Home()
-    df      = h.CurrentState_as_df(last_row=10)
-    #last log entries
-    states  = p.tuplekey_to_nested(df.to_dict())
-    #add time info
-    states.update({"time" : t.strftime("%H:%M",t.gmtime())})
-    #add image path
-    states.update({"file" : {"home_visual"   : "0.JPG",
-                             "motion_energy" : "motion_energy.JPG"}})
-    #get the imagematrix to visualize
-    plt = figure(title="test")
-    plt.rect(df)
-    script, div = components(plt)     
-    
-    states.update({"script" : script,"div" : div})
-    return render(request,'index.html',states)
+	h       = p.Home()
+	df      = h.CurrentState_as_df(last_row=10)
+	#last log entries
+	states  = p.tuplekey_to_nested(df.to_dict())
+	#add time info
+	states.update({"time" : t.strftime("%H:%M",t.gmtime())})
+	#add image path
+	states.update({"file" : {"home_visual"   : "0.JPG",
+					   "motion_energy" : "motion_energy.JPG"}})
+	#get the imagematrix to visualize
+	df.columns= ['_'.join(col).rstrip('_') for col in df.columns.values]
+	df = df.filter(like='state').reset_index()
+	x=[];y=[];state=[]                                                           
+	for i,col in enumerate(df.columns):
+		for ind in df.index:
+			x.append(ind) 
+			y.append(i)
+			state.append(df.iloc[ind,i])
+	import pandas as pd
+	D = pd.DataFrame.from_dict({"x":x,"y":y,"state":state}) 
+	
+	plt 		= figure(title="test")
+	plt.rect(x="x",y="y",width=1,height=1,source=D,fill_color={'field': 'state'})
+	script, div = components(plt)     
+
+	states.update({"script" : script,"div" : div})
+
+	return render(request,'index.html',states)
 
 def interface(request,source):
     #load source's  DF
